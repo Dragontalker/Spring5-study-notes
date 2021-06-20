ccec6a13589f7b4f88a0e195d652b09d4349c35d
Richard <richard.yang.tong@gmail.com>
2021-06-20 15:56:48 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/testdemo/TestSpring5.java
+++ spring_5_demo1/src/com/dragontalker/spring5/testdemo/TestSpring5.java
@@ -2,6 +2,7 @@ package com.dragontalker.spring5.testdemo;
 
 import com.dragontalker.spring5.User;
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,12 +11,13 @@ public class TestSpring5 {
     @Test
     public void testAdd() {
         //1. 加载spring配置文件
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        BeanFactory context = new ClassPathXmlApplicationContext("bean1.xml");
 
         //2. 获取配置创建的对象
         User user = context.getBean("user", User.class);
 
         System.out.println(user);
+
         user.add();
     }
 
