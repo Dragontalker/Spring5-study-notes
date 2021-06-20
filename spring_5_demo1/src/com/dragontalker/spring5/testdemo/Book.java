30243393c09b0c53d5c2c5d08773b30c24d10861
Richard <richard.yang.tong@gmail.com>
2021-06-20 16:31:43 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/testdemo/Book.java
+++ spring_5_demo1/src/com/dragontalker/spring5/testdemo/Book.java
@@ -1,4 +1,10 @@
 package com.dragontalker.spring5.testdemo;
 
 public class Book {
+
+    private String bookName;
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
 }
