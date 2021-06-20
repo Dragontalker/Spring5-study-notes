c311dd5137c7079d00650c17ea880b89122ee22f
Richard <richard.yang.tong@gmail.com>
2021-06-20 15:58:41 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/testdemo/TestSpring5.java
+++ spring_5_demo1/src/com/dragontalker/spring5/testdemo/TestSpring5.java
@@ -11,7 +11,12 @@ public class TestSpring5 {
     @Test
     public void testAdd() {
         //1. 加载spring配置文件
-        BeanFactory context = new ClassPathXmlApplicationContext("bean1.xml");
+        //BeanFactory是Spring内部的使用接口, 不提供开发人员使用
+        //BeanFactory context = new ClassPathXmlApplicationContext("bean1.xml");
+
+        //ApplicationContext是BeanFactory的子接口
+        //提供更多更强大的功能, 一般由开发人员进行使用
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
 
         //2. 获取配置创建的对象
         User user = context.getBean("user", User.class);
