a9edcf299dd395465115097bc91936cd392a2619
Richard <richard.yang.tong@gmail.com>
2021-06-20 16:35:23 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/testdemo/Book.java
+++ spring_5_demo1/src/com/dragontalker/spring5/testdemo/Book.java
@@ -7,13 +7,6 @@ public class Book {
 
     private String bookName;
 
-    public Book() {}
-
-    //有参数构造注入
-    public Book(String bookName) {
-        this.bookName = bookName;
-    }
-
     //set方法注入
     public void setBookName(String bookName) {
         this.bookName = bookName;
