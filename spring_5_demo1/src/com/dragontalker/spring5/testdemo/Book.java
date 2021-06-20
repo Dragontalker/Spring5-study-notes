5ef0e8df79d8500165b611d55ee557b3be221fae
Richard <richard.yang.tong@gmail.com>
2021-06-20 16:35:16 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/testdemo/Book.java
+++ spring_5_demo1/src/com/dragontalker/spring5/testdemo/Book.java
@@ -1,5 +1,8 @@
 package com.dragontalker.spring5.testdemo;
 
+/**
+ * 演示使用set方法进行注入属性
+ */
 public class Book {
 
     private String bookName;
@@ -14,13 +17,5 @@ public class Book {
     //set方法注入
     public void setBookName(String bookName) {
         this.bookName = bookName;
-    }
-
-    //原始方式
-    public static void main(String[] args) {
-        Book book = new Book();
-        book.setBookName("abc");
-
-        Book book1 = new Book("def");
     }
 }
