c0648cfb63cb1cc03b26a9fd91018ea336912a84
Richard <richard.yang.tong@gmail.com>
2021-06-20 11:14:33 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/Main.java
+++ spring_5_demo1/src/com/dragontalker/Main.java
@@ -3,6 +3,6 @@ package com.dragontalker;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Spring 5 Framework, Demo 1");
     }
 }
