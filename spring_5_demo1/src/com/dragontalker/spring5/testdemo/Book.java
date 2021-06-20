3292593c5bc8032bb88dc3a8cac059d4e3a8ba64
Richard <richard.yang.tong@gmail.com>
2021-06-20 16:32:23 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/testdemo/Book.java
+++ spring_5_demo1/src/com/dragontalker/spring5/testdemo/Book.java
@@ -7,4 +7,10 @@ public class Book {
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
+
+    //原始方式
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.setBookName("abc");
+    }
 }
