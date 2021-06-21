b720545f3a980b79d9c177117eb43953e677de72
Richard <richard.yang.tong@gmail.com>
2021-06-21 15:15:46 -0400

    Added notes.

--- spring_5_demo3/src/com/dragontalker/spring5/service/UserService.java
+++ spring_5_demo3/src/com/dragontalker/spring5/service/UserService.java
@@ -1,5 +1,6 @@
 package com.dragontalker.spring5.service;
 
+import com.dragontalker.spring5.dao.UserDao;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 //UserService --> userService
 @Service //等同于<bean id="userService" class="..."/>
 public class UserService {
+
+    //定义dao类型属性
+    private UserDao userDao;
 
     public void add() {
         System.out.println("service add...");
