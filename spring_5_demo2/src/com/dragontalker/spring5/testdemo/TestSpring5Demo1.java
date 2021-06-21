837092921be5d9f0ca37502e12547a2908c1f9c8
Richard <richard.yang.tong@gmail.com>
2021-06-20 21:50:23 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/testdemo/TestSpring5Demo1.java
+++ spring_5_demo2/src/com/dragontalker/spring5/testdemo/TestSpring5Demo1.java
@@ -1,4 +1,16 @@
 package com.dragontalker.spring5.testdemo;
 
+import com.dragontalker.spring5.collectiontype.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestSpring5Demo1 {
+
+    @Test
+    public void testCollection() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        Student student = context.getBean("student", Student.class);
+        student.test();
+    }
 }
