5f67bd55e12096f44ec765cf3233d1ce235bcba8
Richard <richard.yang.tong@gmail.com>
2021-06-20 21:11:15 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/bean/Department.java
+++ spring_5_demo1/src/com/dragontalker/spring5/bean/Department.java
@@ -1,4 +1,9 @@
 package com.dragontalker.spring5.bean;
 
 public class Department {
+    private String departmentName;
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
