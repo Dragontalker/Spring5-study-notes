6ecfe487591521971861867f0b9474a6f43fffd9
Richard <richard.yang.tong@gmail.com>
2021-06-21 15:15:00 -0400

    Added notes.

--- spring_5_demo3/src/com/dragontalker/spring5/dao/UserDaoImpl.java
+++ spring_5_demo3/src/com/dragontalker/spring5/dao/UserDaoImpl.java
@@ -1,5 +1,8 @@
 package com.dragontalker.spring5.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserDaoImpl implements UserDao{
     @Override
     public void add() {
--- spring_5_demo3/src/com/dragontalker/spring5/service/UserService.java
+++ spring_5_demo3/src/com/dragontalker/spring5/service/UserService.java
@@ -2,11 +2,12 @@ package com.dragontalker.spring5.service;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 //在注解里面value属性值可以省略不写
 //默认值是类名称, 首字母小写
 //UserService --> userService
-@Repository //等同于<bean id="userService" class="..."/>
+@Service //等同于<bean id="userService" class="..."/>
 public class UserService {
 
     public void add() {
