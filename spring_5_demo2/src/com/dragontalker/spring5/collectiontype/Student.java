aeccf9fbe45435cefc70dfcc506723606bf0809f
Richard <richard.yang.tong@gmail.com>
2021-06-20 21:39:30 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/collectiontype/Student.java
+++ spring_5_demo2/src/com/dragontalker/spring5/collectiontype/Student.java
@@ -1,6 +1,7 @@
 package com.dragontalker.spring5.collectiontype;
 
 import java.util.List;
+import java.util.Map;
 
 public class Student {
     //1. 数组类型的属性
@@ -8,6 +9,9 @@ public class Student {
 
     //2. list集合类型的属性
     private List<String> list;
+
+    //3. map集合类型的属性
+    private Map<String, String> map;
 
     public void setCourses(String[] courses) {
         this.courses = courses;
