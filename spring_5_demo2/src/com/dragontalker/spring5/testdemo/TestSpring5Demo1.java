edf2f158b491260c5e7be9a299d93800f316dc4b
Richard <richard.yang.tong@gmail.com>
2021-06-21 09:57:02 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/testdemo/TestSpring5Demo1.java
+++ spring_5_demo2/src/com/dragontalker/spring5/testdemo/TestSpring5Demo1.java
@@ -2,6 +2,7 @@ package com.dragontalker.spring5.testdemo;
 
 import com.dragontalker.spring5.collectiontype.Book;
 import com.dragontalker.spring5.collectiontype.Student;
+import com.dragontalker.spring5.factorybean.MyBean;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -20,5 +21,12 @@ public class TestSpring5Demo1 {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
         Book book = context.getBean("book", Book.class);
         book.test();
+    }
+
+    @Test
+    public void testFactoryBean() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean3.xml");
+        MyBean myBean = context.getBean("myBean", MyBean.class);
+        System.out.println(myBean);
     }
 }
