4ecff35b1391e893c8b9e8fc3dbd5d9819827600
Richard <richard.yang.tong@gmail.com>
2021-06-21 17:08:55 -0400

    Added notes.

--- spring_5_demo4/src/com/dragontalker/spring5/JDKProxy.java
+++ spring_5_demo4/src/com/dragontalker/spring5/JDKProxy.java
@@ -9,13 +9,14 @@ public class JDKProxy {
     public static void main(String[] args) {
         //创建接口实现类代理对象
         Class[] interfaces = {UserDao.class};
-
+        Proxy.newProxyInstance(JDKProxy.class.getClassLoader(), interfaces, new UserDaoProxy());
     }
 }
 
 //创建代理对象代码
 class UserDaoProxy implements InvocationHandler {
 
+    //增强的逻辑
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         return null;
