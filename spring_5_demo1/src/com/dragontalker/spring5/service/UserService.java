3e26b1cc42eb693556eac1391b8cc487f972e855
Richard <richard.yang.tong@gmail.com>
2021-06-20 21:00:06 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/service/UserService.java
+++ spring_5_demo1/src/com/dragontalker/spring5/service/UserService.java
@@ -5,11 +5,14 @@ import com.dragontalker.spring5.dao.UserDaoImpl;
 
 public class UserService {
 
+    //创建UserDao类型属性, 生成set方法
+    private UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     public void add() {
         System.out.println("service add...");
-
-        //创建UserDao对象
-        UserDao userDao = new UserDaoImpl();
-        userDao.update();
     }
 }
