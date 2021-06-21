e2382e5762dff28620b0f26079a2d959639f860e
Richard <richard.yang.tong@gmail.com>
2021-06-21 15:24:37 -0400

    Added notes.

--- spring_5_demo3/src/com/dragontalker/spring5/service/UserService.java
+++ spring_5_demo3/src/com/dragontalker/spring5/service/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 //在注解里面value属性值可以省略不写
 //默认值是类名称, 首字母小写
 //UserService --> userService
@@ -16,8 +18,11 @@ public class UserService {
     //定义dao类型属性
     //不需要添加set方法
     //添加注入属性注解
-    @Autowired //根据类型注入
-    @Qualifier(value = "userDaoImpl1") //根据名称注入
+//    @Autowired //根据类型注入
+//    @Qualifier(value = "userDaoImpl1") //根据名称注入
+//    private UserDao userDao;
+
+    @Resource //根据类型进行注入
     private UserDao userDao;
 
     public void add() {
