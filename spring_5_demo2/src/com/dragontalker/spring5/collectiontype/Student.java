67c38087aa6c0b2b150dcd52f8419ca7789893c0
Richard <richard.yang.tong@gmail.com>
2021-06-20 21:38:01 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/collectiontype/Student.java
+++ spring_5_demo2/src/com/dragontalker/spring5/collectiontype/Student.java
@@ -3,4 +3,8 @@ package com.dragontalker.spring5.collectiontype;
 public class Student {
     //1. 数组类型的属性
     private String[] courses;
+
+    public void setCourses(String[] courses) {
+        this.courses = courses;
+    }
 }
