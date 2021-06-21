c8617aed851c64933b3c4a94429ba9b1bae1d075
Richard <richard.yang.tong@gmail.com>
2021-06-21 10:05:00 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/factorybean/MyBean.java
+++ spring_5_demo2/src/com/dragontalker/spring5/factorybean/MyBean.java
@@ -1,4 +1,15 @@
 package com.dragontalker.spring5.factorybean;
 
-public class MyBean {
+import org.springframework.beans.factory.FactoryBean;
+
+public class MyBean implements FactoryBean {
+    @Override
+    public Object getObject() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return null;
+    }
 }
