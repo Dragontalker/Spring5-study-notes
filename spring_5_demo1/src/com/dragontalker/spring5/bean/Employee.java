5aec40b6d82ae39fa4cd929b16e08cb406eda019
Richard <richard.yang.tong@gmail.com>
2021-06-20 21:13:22 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/bean/Employee.java
+++ spring_5_demo1/src/com/dragontalker/spring5/bean/Employee.java
@@ -4,11 +4,18 @@ public class Employee {
     private String employeeName;
     private String gender;
 
+    //员工属于某一个部门, 使用对象形式标识
+    private Department department;
+
     public void setEmployeeName(String employeeName) {
         this.employeeName = employeeName;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 }
