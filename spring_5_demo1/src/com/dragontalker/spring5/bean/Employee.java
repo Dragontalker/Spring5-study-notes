65322cd088401240a1926c64e6feffd8efdc0f6c
Richard <richard.yang.tong@gmail.com>
2021-06-20 21:11:58 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/bean/Employee.java
+++ spring_5_demo1/src/com/dragontalker/spring5/bean/Employee.java
@@ -1,4 +1,14 @@
 package com.dragontalker.spring5.bean;
 
 public class Employee {
+    private String employeeName;
+    private String gender;
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
