66ba5ec4b2bfc71fa50ab4039dcb1b82cd02eb4b
Richard <richard.yang.tong@gmail.com>
2021-06-20 20:54:59 -0400

    Added notes.

--- spring_5_demo1/src/com/dragontalker/spring5/dao/UserDao.java
+++ spring_5_demo1/src/com/dragontalker/spring5/dao/UserDao.java
@@ -1,4 +1,6 @@
 package com.dragontalker.spring5.dao;
 
 public interface UserDao {
+
+    public void update();
 }
--- spring_5_demo1/src/com/dragontalker/spring5/dao/UserDaoImpl.java
+++ spring_5_demo1/src/com/dragontalker/spring5/dao/UserDaoImpl.java
@@ -1,4 +1,8 @@
 package com.dragontalker.spring5.dao;
 
 public class UserDaoImpl implements UserDao{
+    @Override
+    public void update() {
+        System.out.println("dao update...");
+    }
 }
