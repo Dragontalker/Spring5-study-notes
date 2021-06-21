c4a2bf636cdb5b9cb035dff8161375c5f11d067c
Richard <richard.yang.tong@gmail.com>
2021-06-21 12:09:17 -0400

    Added notes.

--- spring_5_demo3/src/com/dragontalker/spring5/service/UserService.java
+++ spring_5_demo3/src/com/dragontalker/spring5/service/UserService.java
@@ -1,5 +1,11 @@
 package com.dragontalker.spring5.service;
 
+import org.springframework.stereotype.Component;
+
+//在注解里面value属性值可以省略不写
+//默认值是类名称, 首字母小写
+//UserService --> userService
+@Component(value = "userService") //等同于<bean id="userService" class="..."/>
 public class UserService {
 
     public void add() {
