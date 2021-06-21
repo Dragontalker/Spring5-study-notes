d93953b50c9a01f99231a10598a34b85afd36ad4
Richard <richard.yang.tong@gmail.com>
2021-06-21 18:18:16 -0400

    Added notes.

--- spring_5_demo4/src/com/dragontalker/spring5/UserDaoImpl.java
+++ spring_5_demo4/src/com/dragontalker/spring5/UserDaoImpl.java
@@ -3,11 +3,13 @@ package com.dragontalker.spring5;
 public class UserDaoImpl implements UserDao{
     @Override
     public int add(int a, int b) {
+        System.out.println("add方法执行了...");
         return a + b;
     }
 
     @Override
     public String update(String id) {
+        System.out.println("update方法执行了...");
         return id;
     }
 }
