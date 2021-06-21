9eb84e60dedeaea7a2d72c959e66eafd3883aa76
Richard <richard.yang.tong@gmail.com>
2021-06-21 09:33:49 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/collectiontype/Course.java
+++ spring_5_demo2/src/com/dragontalker/spring5/collectiontype/Course.java
@@ -1,4 +1,9 @@
 package com.dragontalker.spring5.collectiontype;
 
 public class Course {
+    private String courseName;
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
 }
