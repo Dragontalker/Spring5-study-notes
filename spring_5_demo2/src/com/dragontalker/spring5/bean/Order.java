5a12761899c8ddbd24fcd7bd08fb1edcdd03fa43
Richard <richard.yang.tong@gmail.com>
2021-06-21 10:37:24 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/bean/Order.java
+++ spring_5_demo2/src/com/dragontalker/spring5/bean/Order.java
@@ -1,4 +1,10 @@
 package com.dragontalker.spring5.bean;
 
 public class Order {
+
+    private String orderName;
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
 }
