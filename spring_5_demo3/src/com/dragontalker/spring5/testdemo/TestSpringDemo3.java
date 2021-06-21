62d474245c7ad922490b08cca666919de4588ac9
Richard <richard.yang.tong@gmail.com>
2021-06-21 15:44:56 -0400

    Added notes.

--- spring_5_demo3/src/com/dragontalker/spring5/testdemo/TestSpringDemo3.java
+++ spring_5_demo3/src/com/dragontalker/spring5/testdemo/TestSpringDemo3.java
@@ -1,8 +1,10 @@
 package com.dragontalker.spring5.testdemo;
 
+import com.dragontalker.spring5.config.SpringConfig;
 import com.dragontalker.spring5.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpringDemo3 {
@@ -10,6 +12,15 @@ public class TestSpringDemo3 {
     @Test
     public void testComponent() {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        System.out.println(userService);
+        userService.add();
+    }
+
+    @Test
+    public void testSpringConfig() {
+        //加载配置类
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         System.out.println(userService);
         userService.add();
