5c993e6d1677d3e710556fd262d3ce8db28b74ff
Richard <richard.yang.tong@gmail.com>
2021-06-21 13:09:56 -0400

    Added notes.

--- spring_5_demo3/src/com/dragontalker/spring5/testdemo/TestSpringDemo3.java
+++ spring_5_demo3/src/com/dragontalker/spring5/testdemo/TestSpringDemo3.java
@@ -1,11 +1,17 @@
 package com.dragontalker.spring5.testdemo;
 
+import com.dragontalker.spring5.service.UserService;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpringDemo3 {
 
     @Test
     public void testComponent() {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        System.out.println(userService);
+        userService.add();
     }
 }
