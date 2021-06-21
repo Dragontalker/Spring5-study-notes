353d610294e3d0663037b5cc839219ed5f243f43
Richard <richard.yang.tong@gmail.com>
2021-06-21 11:16:13 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/autoweb/Employee.java
+++ spring_5_demo2/src/com/dragontalker/spring5/autoweb/Employee.java
@@ -1,4 +1,16 @@
 package com.dragontalker.spring5.autoweb;
 
 public class Employee {
+    private Department department;
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "department=" + department +
+                '}';
+    }
 }
