d0a378496a976c7f5b1aba88333be9d8e5360b03
Richard <richard.yang.tong@gmail.com>
2021-06-21 10:40:28 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/bean/Order.java
+++ spring_5_demo2/src/com/dragontalker/spring5/bean/Order.java
@@ -4,6 +4,11 @@ public class Order {
 
     private String orderName;
 
+    //无参数构造器
+    public Order() {
+        System.out.println("Step 1: 执行无参数构造器创建bean实例");
+    }
+
     public void setOrderName(String orderName) {
         this.orderName = orderName;
     }
