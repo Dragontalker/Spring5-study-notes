81782ff3efa8d180008ffc322ccb45bb231ab0fa
Richard <richard.yang.tong@gmail.com>
2021-06-21 15:13:36 -0400

    Added notes.

--- spring_5_demo3/src/com/dragontalker/spring5/dao/UserDaoImpl.java
+++ spring_5_demo3/src/com/dragontalker/spring5/dao/UserDaoImpl.java
@@ -1,4 +1,9 @@
 package com.dragontalker.spring5.dao;
 
 public class UserDaoImpl implements UserDao{
+
+    @Override
+    public void add() {
+        System.out.println("service add...");
+    }
 }
