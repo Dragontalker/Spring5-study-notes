0d456c7612959e0e6743dc73d38522cf788c1f6e
Richard <richard.yang.tong@gmail.com>
2021-06-21 19:56:44 -0400

    Added notes.

--- spring_5_demo4/src/com/dragontalker/spring5/aopanno/User.java
+++ spring_5_demo4/src/com/dragontalker/spring5/aopanno/User.java
@@ -1,5 +1,6 @@
 package com.dragontalker.spring5.aopanno;
 
+//被增强的类
 public class User {
 
     public void add() {
--- spring_5_demo4/src/com/dragontalker/spring5/aopanno/UserProxy.java
+++ spring_5_demo4/src/com/dragontalker/spring5/aopanno/UserProxy.java
@@ -1,7 +1,9 @@
 package com.dragontalker.spring5.aopanno;
 
+//增强的类
 public class UserProxy {
 
+    //前置通知
     public void before() {
         System.out.println("before...");
     }
