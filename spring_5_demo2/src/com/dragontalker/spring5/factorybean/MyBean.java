9f109103313999848147e700168c4bb30bb1f6fe
Richard <richard.yang.tong@gmail.com>
2021-06-21 10:06:18 -0400

    Added notes.

--- spring_5_demo2/src/com/dragontalker/spring5/factorybean/MyBean.java
+++ spring_5_demo2/src/com/dragontalker/spring5/factorybean/MyBean.java
@@ -1,15 +1,25 @@
 package com.dragontalker.spring5.factorybean;
 
+import com.dragontalker.spring5.collectiontype.Course;
 import org.springframework.beans.factory.FactoryBean;
 
-public class MyBean implements FactoryBean {
+public class MyBean implements FactoryBean<Course> {
+
+    //定义返回bean
     @Override
-    public Object getObject() throws Exception {
+    public Course getObject() throws Exception {
+        Course course = new Course();
+        course.setCourseName("abc");
         return null;
     }
 
     @Override
     public Class<?> getObjectType() {
         return null;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return FactoryBean.super.isSingleton();
     }
 }
