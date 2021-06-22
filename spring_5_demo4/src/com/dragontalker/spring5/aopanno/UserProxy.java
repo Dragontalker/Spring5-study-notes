a97348070c3769dfc89b8fb636760fafc7362698
Richard <richard.yang.tong@gmail.com>
2021-06-21 20:04:09 -0400

    Added notes.

--- spring_5_demo4/src/com/dragontalker/spring5/aopanno/UserProxy.java
+++ spring_5_demo4/src/com/dragontalker/spring5/aopanno/UserProxy.java
@@ -1,9 +1,11 @@
 package com.dragontalker.spring5.aopanno;
 
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 //增强的类
 @Component
+@Aspect //生成代理对象
 public class UserProxy {
 
     //前置通知
