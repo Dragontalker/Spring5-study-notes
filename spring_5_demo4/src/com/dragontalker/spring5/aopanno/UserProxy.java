c14c2bc0525f18e78f5f63293167b11d0c3aba90
Richard <richard.yang.tong@gmail.com>
2021-06-21 20:27:44 -0400

    Added notes.

--- spring_5_demo4/src/com/dragontalker/spring5/aopanno/UserProxy.java
+++ spring_5_demo4/src/com/dragontalker/spring5/aopanno/UserProxy.java
@@ -9,33 +9,37 @@ import org.springframework.stereotype.Component;
 @Aspect //生成代理对象
 public class UserProxy {
 
+    //相同切入点抽取
+    @Pointcut(value = "execution(* com.dragontalker.spring5.aopanno.User.add())")
+    public void pointDemo() {}
+
     //前置通知
     //@Before注解表示作为前置通知
-    @Before(value = "execution(* com.dragontalker.spring5.aopanno.User.add())")
+    @Before(value = "pointDemo()")
     public void before() {
         System.out.println("before...");
     }
 
     //返回通知, 和AfterThrowing不能同时存在
-    @AfterReturning(value = "execution(* com.dragontalker.spring5.aopanno.User.add())")
+    @AfterReturning(value = "pointDemo()")
     public void afterReturning() {
         System.out.println("afterReturning...");
     }
 
     //异常通知
-    @AfterThrowing(value = "execution(* com.dragontalker.spring5.aopanno.User.add())")
+    @AfterThrowing(value = "pointDemo()")
     public void afterThrowing() {
         System.out.println("afterThrowing...");
     }
 
     //最终通知, 类似finally
-    @After(value = "execution(* com.dragontalker.spring5.aopanno.User.add())")
+    @After(value = "pointDemo()")
     public void after() {
         System.out.println("after...");
     }
 
     //环绕通知
-    @Around(value = "execution(* com.dragontalker.spring5.aopanno.User.add())")
+    @Around(value = "pointDemo()")
     public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("before around...");
 
