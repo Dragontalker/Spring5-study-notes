8bcdf05db15322a905e469c2137c36296e4870f2
Richard <richard.yang.tong@gmail.com>
2021-06-22 10:36:21 -0400

    Added notes.

--- spring_5_demo5/src/com/dragontalker/spring5/entity/User.java
+++ spring_5_demo5/src/com/dragontalker/spring5/entity/User.java
@@ -4,4 +4,28 @@ public class User {
     private String userId;
     private String userName;
     private String userStatus;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
 }
