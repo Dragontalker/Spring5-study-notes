301131c3e7c83330f7963e01552ff20e77b0a278
Richard <richard.yang.tong@gmail.com>
2021-06-22 13:42:46 -0400

    Added notes.

--- spring_5_txdemo1/src/com/dragontalker/spring5/dao/UserDaoImpl.java
+++ spring_5_txdemo1/src/com/dragontalker/spring5/dao/UserDaoImpl.java
@@ -1,7 +1,12 @@
 package com.dragontalker.spring5.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDaoImpl implements UserDao{
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 }
