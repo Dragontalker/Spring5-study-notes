60dfa48dbfe20203f7c43ea70cdf54fcc740ce4f
Richard <richard.yang.tong@gmail.com>
2021-06-21 20:30:26 -0400

    Added notes.

--- spring_5_demo4/src/com/dragontalker/spring5/aopanno/PersonProxy.java
+++ spring_5_demo4/src/com/dragontalker/spring5/aopanno/PersonProxy.java
@@ -1,4 +1,17 @@
 package com.dragontalker.spring5.aopanno;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
 public class PersonProxy {
+
+    //前置通知
+    //@Before注解表示作为前置通知
+    @Before(value = "execution(* com.dragontalker.spring5.aopanno.User.add())")
+    public void before() {
+        System.out.println("before...");
+    }
 }
