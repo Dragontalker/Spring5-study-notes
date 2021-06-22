3d72414a8dcdf2129be1b0214d9b5345bb331f4d
Richard <richard.yang.tong@gmail.com>
2021-06-22 15:51:13 -0400

    Added notes.

--- spring_5_txdemo1/src/com/dragontalker/spring5/config/TxConfig.java
+++ spring_5_txdemo1/src/com/dragontalker/spring5/config/TxConfig.java
@@ -4,8 +4,10 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Properties;
@@ -24,5 +26,16 @@ public class TxConfig {
         dataSource.setUsername("root");
         dataSource.setPassword("password");
         return dataSource;
+    }
+
+    //创建JdbcTemplate对象
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+        //到ioc容器中根据类型找到dataSource, 完成注入
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+        //注入dataSource
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
     }
 }
