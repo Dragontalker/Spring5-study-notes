8d488b9ee7efbd4a69d61eedaa6eea4e3a2b8080
Richard <richard.yang.tong@gmail.com>
2021-06-22 15:48:54 -0400

    Added notes.

--- spring_5_txdemo1/src/com/dragontalker/spring5/config/TxConfig.java
+++ spring_5_txdemo1/src/com/dragontalker/spring5/config/TxConfig.java
@@ -1,9 +1,28 @@
 package com.dragontalker.spring5.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "com.dragontalker")
+@EnableTransactionManagement //开启事务
 public class TxConfig {
+
+    //创建数据库连接池
+    @Bean
+    public DruidDataSource getDruidDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql:///user_db");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+        return dataSource;
+    }
 }
