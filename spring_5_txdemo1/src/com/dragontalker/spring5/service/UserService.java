3d1b20ce8f91ca39c4cde0638b7948298129766c
Richard <richard.yang.tong@gmail.com>
2021-06-22 13:42:24 -0400

    Added notes.

--- spring_5_txdemo1/src/com/dragontalker/spring5/service/UserService.java
+++ spring_5_txdemo1/src/com/dragontalker/spring5/service/UserService.java
@@ -1,7 +1,13 @@
 package com.dragontalker.spring5.service;
 
+import com.dragontalker.spring5.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    //注入dao
+    @Autowired
+    private UserDao userDao;
 }
