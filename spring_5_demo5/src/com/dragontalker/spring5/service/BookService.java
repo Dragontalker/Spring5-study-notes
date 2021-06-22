ef199c3a43b56d7ce77627503b5c17b334a58c6b
Richard <richard.yang.tong@gmail.com>
2021-06-22 10:26:28 -0400

    Added notes.

--- spring_5_demo5/src/com/dragontalker/spring5/service/BookService.java
+++ spring_5_demo5/src/com/dragontalker/spring5/service/BookService.java
@@ -1,7 +1,13 @@
 package com.dragontalker.spring5.service;
 
+import com.dragontalker.spring5.dao.BookDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+
+    //注入dao
+    @Autowired
+    private BookDao bookDao;
 }
