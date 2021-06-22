14939ab3641949e1e45f38d18012e1cbf3fcc24f
Richard <richard.yang.tong@gmail.com>
2021-06-22 13:56:07 -0400

    Added notes.

--- spring_5_txdemo1/src/com/dragontalker/spring5/test/TestUser.java
+++ spring_5_txdemo1/src/com/dragontalker/spring5/test/TestUser.java
@@ -1,11 +1,16 @@
 package com.dragontalker.spring5.test;
 
+import com.dragontalker.spring5.service.UserService;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestUser {
 
     @Test
     public void testTx() {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.accountMoney();
     }
 }
