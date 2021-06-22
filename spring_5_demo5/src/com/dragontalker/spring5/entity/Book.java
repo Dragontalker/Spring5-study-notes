2529ffaa580c1d52242d23255c4ba35903d0e505
Richard <richard.yang.tong@gmail.com>
2021-06-22 10:39:08 -0400

    Added notes.

--- spring_5_demo5/src/com/dragontalker/spring5/entity/Book.java
+++ spring_5_demo5/src/com/dragontalker/spring5/entity/Book.java
@@ -5,5 +5,27 @@ public class Book {
     private String bookName;
     private String bookStatus;
 
+    public String getBookId() {
+        return bookId;
+    }
 
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(String bookStatus) {
+        this.bookStatus = bookStatus;
+    }
 }
