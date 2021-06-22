d419d16ee9e2c03f2812565cb8a7b8a39c850106
Richard <richard.yang.tong@gmail.com>
2021-06-22 15:43:58 -0400

    Added notes.

--- spring_5_txdemo1/src/com/dragontalker/spring5/config/TxConfig.java
+++ spring_5_txdemo1/src/com/dragontalker/spring5/config/TxConfig.java
@@ -1,4 +1,9 @@
 package com.dragontalker.spring5.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.dragontalker")
 public class TxConfig {
 }
