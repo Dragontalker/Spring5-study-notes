d6c3cf2640d2b8797c8c0f768a5307ea16246c2c
Richard <richard.yang.tong@gmail.com>
2021-06-22 16:14:14 -0400

    Added notes.

--- spring_5_txdemo1/src/com/dragontalker/spring5/test/UserLog.java
+++ spring_5_txdemo1/src/com/dragontalker/spring5/test/UserLog.java
@@ -1,4 +1,15 @@
 package com.dragontalker.spring5.test;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class UserLog {
+
+    private static final Logger log = LoggerFactory.getLogger(UserLog.class);
+
+    public static void main(String[] args) {
+        log.info("Hello Log4J2!");
+        log.warn("Warning!!!!");
+    }
 }
