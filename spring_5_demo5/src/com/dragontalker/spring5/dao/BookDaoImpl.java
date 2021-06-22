8a52cbe4e46e714f8c68638b86054149e9416e12
Richard <richard.yang.tong@gmail.com>
2021-06-22 10:28:41 -0400

    Added notes.

--- spring_5_demo5/src/com/dragontalker/spring5/dao/BookDaoImpl.java
+++ spring_5_demo5/src/com/dragontalker/spring5/dao/BookDaoImpl.java
@@ -1,7 +1,13 @@
 package com.dragontalker.spring5.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookDaoImpl implements BookDao{
+
+    //注入JdbcTemplate
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 }
