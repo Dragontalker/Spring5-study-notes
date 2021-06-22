5c8fec3b56590fd07f421dd5539a9c5918583f9c
Richard <richard.yang.tong@gmail.com>
2021-06-22 14:04:31 -0400

    Added notes.

--- spring_5_txdemo1/src/com/dragontalker/spring5/service/UserService.java
+++ spring_5_txdemo1/src/com/dragontalker/spring5/service/UserService.java
@@ -4,6 +4,8 @@ import com.dragontalker.spring5.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.ParameterizedType;
+
 @Service
 public class UserService {
 
@@ -13,10 +15,21 @@ public class UserService {
 
     //转账的方法
     public void accountMoney() {
-        //lucy少100
-        userDao.reduceMoney();
+        try {
+            //1. 开始事务
 
-        //mary多100
-        userDao.addMoney();
+            //2. 进行业务操作
+            //lucy少100
+            userDao.reduceMoney();
+
+            //mary多100
+            userDao.addMoney();
+
+            //3. 没有发生异常, 提交事务
+        } catch (Exception e) {
+            e.printStackTrace();
+            //4. 出现异常, 事务回滚
+        }
+
     }
 }
