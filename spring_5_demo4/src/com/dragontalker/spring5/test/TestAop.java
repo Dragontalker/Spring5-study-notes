d09da4296e182b6a241c83aeb9f488dd6d6a47cc
Richard <richard.yang.tong@gmail.com>
2021-06-21 20:45:34 -0400

    Added notes.

--- spring_5_demo4/src/com/dragontalker/spring5/test/TestAop.java
+++ spring_5_demo4/src/com/dragontalker/spring5/test/TestAop.java
@@ -1,6 +1,7 @@
 package com.dragontalker.spring5.test;
 
 import com.dragontalker.spring5.aopanno.User;
+import com.dragontalker.spring5.aopxml.Book;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,5 +13,12 @@ public class TestAop {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         User user = context.getBean("user", User.class);
         user.add();
+    }
+
+    @Test
+    public void testAopXml() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        Book book = context.getBean("book", Book.class);
+        book.buy();
     }
 }
